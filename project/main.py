41a9101b23ebe27eda7d0c9e66e1d7806b4f402b
hhaa1382 <mehranfarhamid82@gmail.com>
2023-12-06 22:59:07 +0330

    value iteration algorithm add

--- project/main.py
+++ project/main.py
@@ -7,10 +7,16 @@ import random
 V = np.zeros(shape=48)
 Q = np.zeros(shape=(48, 4))
 discount_factory = 0.9
+states_num = 48
+action_num = 4
+policy = np.zeros(shape=states_num)
 
 
 def SetQTable(state, next_state, prob, action, reward):
-    pass
+    r = reward
+    if state == next_state:
+        r = -100
+    Q[state][action] = prob * (r + discount_factory * V[next_state])
 
 
 if __name__ == '__main__':
@@ -23,6 +29,19 @@ if __name__ == '__main__':
 
     for key , value in env.P.items():
         print("key : "+str(key)+" --- value : "+str(value))
+    P_values = env.P
+
+    for __ in range(500):
+        V_old=V.copy()
+        for s in range(states_num):
+            q_values = np.zeros(action_num)
+            for a in range(action_num):
+                for prob, next_state, reward, _ in P_values[s][a]:
+                    # print("prob : "+str(prob)+" --- next_state : "+str(next_state)+" --- reward : "+str(reward))
+                    # input()
+                    q_values[a] += prob * (reward + discount_factory * V_old[next_state])
+            V[s] = max(q_values)
+            policy[s] = np.argmax(q_values)
 
     for __ in range(max_iter_number):
         # TODO: Implement the agent policy here
