bc674df120d0f7bc0f290e639258c98217d63a1c
hhaa1382 <mehranfarhamid82@gmail.com>
2023-12-11 22:59:49 +0330

    q_values formula updated

--- project/main.py
+++ project/main.py
@@ -4,7 +4,6 @@ import numpy as np
 import random
 
 V = np.zeros(shape=48)
-Q = np.zeros(shape=(48, 4))
 discount_factory = 0.9
 states_num = 48
 action_num = 4
@@ -13,38 +12,27 @@ end_state = 47
 policy = np.zeros(shape=states_num)
 
 
-def getReward(next_state, current_state, r):
-    re = r
-    if next_state == end_state:
-        re = 200
-    elif next_state == current_state:
-        re = -100
-    return re
-
-
-def check_worst(q_values):
-    if q_values[0] == q_values[2] or q_values[1] == q_values[3]:
-        if q_values[0] <= q_values[1]:
-            return 0
-        elif q_values[0] > q_values[1]:
-            return 1
-    else:
-        return np.argmax(q_values)
-
-
-def optimal_policy(P):
+def optimal_policy(env):
     V_old = np.ones(shape=states_num)
+    P = env.P
+    V[end_state] = 4000
 
-    while np.abs(V - V_old).max() != 0:
+    for __ in range(2000):
         V_old = V.copy()
         for s in range(states_num):
-            q_values = np.zeros(action_num)
-            for a in range(action_num):
-                for prob, next_state, reward, _ in P[s][a]:
-                    q_values[a] += prob * (getReward(next_state, s, reward) + discount_factory * V_old[next_state])
-            V[s] = max(q_values)
-            policy[s] = np.argmax(q_values)
-            # policy[s] = check_worst(q_values)
+            q_values = np.zeros(shape=action_num)
+            for q in range(action_num):
+                if s != end_state:
+                    for a in range(action_num):
+                        if abs(q-a)!=2:
+                            P_value = P[s][a][0]
+                            next_state = P_value[1]
+                            reward = P_value[2]
+
+                            q_values[q] += (1/3) * (reward + discount_factory * V_old[next_state])
+            if s != end_state:
+                V[s] = max(q_values)
+                policy[s] = np.argmax(q_values)
 
 
 if __name__ == '__main__':
@@ -55,7 +43,7 @@ if __name__ == '__main__':
     # Define the maximum number of iterations
     max_iter_number = 1000
 
-    optimal_policy(env.P)
+    optimal_policy(env)
 
     turns = ["Up", "Right", "Down", "Left"]
 
